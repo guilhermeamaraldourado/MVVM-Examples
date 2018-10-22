85fba303ac120ed6d0d5fe9b0795d912cf6fb603
Wooba Sistemas <woobasistemas@gmail.com>
2018-10-22 14:29:26 -0300

    Initial Commit

---
+++ MVVM 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM 1
+//
+//  Created by WOOBA-MAC1 on 22/10/18.
+//  Copyright © 2018 WOOBA-MAC1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
